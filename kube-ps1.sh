6066c0c70f02c993fb0c27aae1519b0cef0b8779
Jon Mosco <jonny.mosco@gmail.com>
2018-02-06 21:45:49 -0500

    Add remaining KUBE_PS1_RESET_COLOR to builder function

--- kube-ps1.sh
+++ kube-ps1.sh
@@ -357,14 +357,14 @@ kube_ps1() {
   fi
 
   # Context
-  KUBE_PS1+="$(_kube_ps1_color_fg $KUBE_PS1_CTX_COLOR)${KUBE_PS1_CONTEXT}$(_kube_ps1_color_fg reset_color)"
+  KUBE_PS1+="$(_kube_ps1_color_fg $KUBE_PS1_CTX_COLOR)${KUBE_PS1_CONTEXT}${KUBE_PS1_RESET_COLOR}"
 
   # Namespace
   if [[ "${KUBE_PS1_NS_ENABLE}" == true ]]; then
     if [[ -n "${KUBE_PS1_DIVIDER}" ]]; then
       KUBE_PS1+="${KUBE_PS1_DIVIDER}"
     fi
-    KUBE_PS1+="$(_kube_ps1_color_fg ${KUBE_PS1_NS_COLOR})${KUBE_PS1_NAMESPACE}$(_kube_ps1_color_fg reset_color)"
+    KUBE_PS1+="$(_kube_ps1_color_fg ${KUBE_PS1_NS_COLOR})${KUBE_PS1_NAMESPACE}${KUBE_PS1_RESET_COLOR}"
   fi
 
   # Suffix
