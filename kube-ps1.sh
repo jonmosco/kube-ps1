a64a4edb0bb35938f66502b67b4a8b472839cde1
Antonio M. Macías Ojeda <antonio.macias.ojeda@gmail.com>
2018-04-04 09:50:46 +0300

    Better way to disable/rename default namespace

--- kube-ps1.sh
+++ kube-ps1.sh
@@ -27,7 +27,7 @@ KUBE_PS1_SYMBOL_ENABLE="${KUBE_PS1_SYMBOL_ENABLE:-true}"
 KUBE_PS1_SYMBOL_DEFAULT="${KUBE_PS1_SYMBOL_DEFAULT:-\u2388 }"
 KUBE_PS1_SYMBOL_USE_IMG="${KUBE_PS1_SYMBOL_USE_IMG:-false}"
 KUBE_PS1_NS_ENABLE="${KUBE_PS1_NS_ENABLE:-true}"
-KUBE_PS1_NS_DEFAULT_ENABLE="${KUBE_PS1_NS_DEFAULT_ENABLE:-true}"
+KUBE_PS1_NS_DEFAULT_STRING="${KUBE_PS1_NS_DEFAULT_STRING:-default}"
 KUBE_PS1_PREFIX="${KUBE_PS1_PREFIX-(}"
 KUBE_PS1_SEPARATOR="${KUBE_PS1_SEPARATOR-|}"
 KUBE_PS1_DIVIDER="${KUBE_PS1_DIVIDER-:}"
@@ -245,8 +245,8 @@ _kube_ps1_get_context_ns() {
     # Set namespace to 'default' if it is not defined
     KUBE_PS1_NAMESPACE="${KUBE_PS1_NAMESPACE:-default}"
     # If default is not desired, empty string in that case
-    if [[ "${KUBE_PS1_NS_DEFAULT_ENABLE}" == false && "${KUBE_PS1_NAMESPACE}" == "default" ]]; then
-      KUBE_PS1_NAMESPACE=""
+    if [[ "${KUBE_PS1_NAMESPACE}" == "default" ]]; then
+      KUBE_PS1_NAMESPACE="${KUBE_PS1_NS_DEFAULT_STRING}"
     fi
   fi
 }
